9a8f8095176e9482e30b965378d2f4da9b95ac51
Irina Korobeinikova <macuser@MacBook-Pro.local>
2026-03-24 19:30:11 +0300

    accounts-service application skeleton added

---
+++ accounts-service/src/test/java/ru/practicum/template/AccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
