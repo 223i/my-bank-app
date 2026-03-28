62933476bbe9aa4e411d218f31ad9cda186d4537
Irina Korobeinikova <macuser@MacBook-Pro.local>
2026-03-28 18:47:55 +0300

    consul dependencies added

--- gateway-service/src/main/java/com/iron/GatewayApplication.java
+++ gateway-service/src/main/java/com/iron/GatewayApplication.java
@@ -2,10 +2,13 @@ package com.iron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+
     }
 }
